ab15e74e2fbecfb3277d90138c2eb42daf8a3480
Abdul Ghani <ghanisheikh26@gmail.com>
2026-03-28 14:48:45 +0530

    AI chat in CLI

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,30 @@
 import { app } from "./src/app.js";
 import "dotenv/config";
 import { connectToDB } from "./src/configs/database.config.js";
+import { ask } from "../backend/src/services/ai.service.js";
+import readline from "readline/promises";
+import { HumanMessage } from "@langchain/core/messages";
 
 connectToDB();
+
+const rl = new readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let messages = [];
+
+while(true) {
+    const userInput = await rl.question("You: ");
+
+    if(userInput === "/bye") break;
+    messages.push(new HumanMessage(userInput));
+
+    const response = await ask(messages);
+
+    messages.push(response);
+    console.log(`AI: ${response.content}`);
+}
 
 app.listen(process.env.PORT, () => {
     console.log(`Server on ${process.env.PORT}`);
--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -2,10 +2,13 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import "dotenv/config";
 
 const model = new ChatGoogleGenerativeAI({
-    model: "gemini-2.5-flash-lite",
-    apiKey: process.env.GOOGLE_GEMINI_API_KEY,
+    model: "gemini-2.5-flash",
+    apiKey: process.env.GEMINI_API_KEY,
     temperature: 2,
     maxRetries: 2
 });
 
-export default model;
+export async function ask(query) {
+    const response = await model.invoke(query);
+    return response;
+}
