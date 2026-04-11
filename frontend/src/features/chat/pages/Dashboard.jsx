bfc2069d24db68aee46a70b8be24fb3d18b9650a
Abdul Ghani <ghanisheikh26@gmail.com>
2026-04-12 04:00:54 +0530

    creating chat ui

--- frontend/src/features/chat/pages/Dashboard.jsx
+++ frontend/src/features/chat/pages/Dashboard.jsx
@@ -25,7 +25,6 @@ const Dashboard = () => {
         <div className="w-[25%] py-3 flex flex-col gap-10 bg-neutral-900 rounded-md">
             
             <Navbar click={handleLogoutClick} />
-
             <div className="chats flex flex-col gap-1 px-3">
                 <div className="chat hover:scale-101 duration-300 ease-in-out cursor-pointer w-full rounded-md bg-neutral-800 px-5 py-1.5">
                     <p>Chat title</p>
@@ -34,14 +33,14 @@ const Dashboard = () => {
                     <p>Chat title</p>
                 </div>
             </div>
+
         </div>
 
-        <div className="chatting w-[75%] relative py-5 px-6 flex flex-col gap-3">
+        <div className="chatting min-h-screen overflow-y-auto w-[75%] relative py-5 px-6 flex flex-col gap-3">
 
             <div className="user p-2 rounded-lg w-fit self-end bg-neutral-900 text-sm">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, soluta?
             </div>
-
             <div className="ai p-2 rounded-lg w-3/4 bg-neutral-900 text-sm">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates aperiam reiciendis nostrum a nisi numquam nihil, quibusdam voluptatum hic! Optio pariatur quis nesciunt ipsa cum nemo aspernatur illum unde, exercitationem consequatur. Quaerat cupiditate eveniet a dolor corrupti praesentium quas quia tempora itaque hic, laudantium sapiente, voluptatem sed modi deleniti ratione!
             </div>
@@ -50,6 +49,7 @@ const Dashboard = () => {
                 <input type="text" placeholder="Type your message" className="w-[90%] text-sm bg-neutral-950 outline-none border-none p-2 rounded-lg" />
                 <button className="py-1 shadow-md shadow-black/50 active:shadow-none duration-300 ease-in-out rounded-md bg-neutral-950 w-[10%] cursor-pointer px-4">Send</button>
             </div>
+
         </div>
     </main>
 }
