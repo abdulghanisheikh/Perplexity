9ce519d50b9e6c94a3b3a7fef1eda98db13ee1e9
Abdul Ghani <ghanisheikh26@gmail.com>
2026-05-13 16:43:48 +0530

    changed user input box design

--- frontend/src/features/chat/components/InputBox.jsx
+++ frontend/src/features/chat/components/InputBox.jsx
@@ -3,14 +3,14 @@ import { IoMdArrowRoundUp } from "react-icons/io";
 const InputBox = ({handleSendMessage, userMessage, setUserMessage, loading}) => {
   return (
     <footer>
-        <form onSubmit={handleSendMessage} className="userInput w-2/3 flex gap-3 justify-center rounded-lg fixed bottom-5 right-15 px-3">
-            <input type="text" value={userMessage} onChange={(e) => setUserMessage(e.target.value)} placeholder="Type your message here..." className="w-[85%] py-3 text-sm bg-neutral-950 border border-white/50 outline-none hover:bg-neutral-900 duration-300 ease-in-out px-2 rounded-lg" />
+        <form onSubmit={handleSendMessage} className="userInput bg-neutral-950 border border-white/50 w-2/3 flex items-center justify-between px-5 py-2 rounded-full fixed bottom-5 right-15">
+            <input type="text" value={userMessage} onChange={(e) => setUserMessage(e.target.value)} placeholder="Type your message here..." className="w-[85%] text-sm outline-none" />
             
-            <button disabled={!userMessage.trim() || loading} type="submit" className={`border border-white/50 rounded-full p-2.5 text-sky-500 bg-neutral-950
+            <button disabled={!userMessage.trim() || loading} type="submit" className={`rounded-full p-1.5 text-sky-500 bg-sky-600
             ${!userMessage.trim() || loading ?
-            'opacity-50' : 
+            'opacity-50' :
             'cursor-pointer active:scale-90 duration-300 ease-in-out'}`}>
-                <IoMdArrowRoundUp size={30}/>
+                <IoMdArrowRoundUp color="white" size={30}/>
             </button>
         </form>
     </footer>
--- frontend/src/features/chat/components/WelcomeScreen.jsx
+++ frontend/src/features/chat/components/WelcomeScreen.jsx
@@ -13,14 +13,14 @@ const WelcomeScreen = ({ username, handleSendMessageClick, userMessage, setUserM
 
 		<h1 className="font-semibold text-6xl">Welcome to <span className="font-bold text-sky-500">Perplexity</span></h1>
 
-		<form onSubmit={handleSendMessageClick} className="userInput mt-8 flex items-center justify-center w-180 gap-4">
-			<textarea type="text" value={userMessage} onChange={(e) => setUserMessage(e.target.value)} placeholder="How can I help you today?" className="py-3 h-35 w-150 text-sm bg-neutral-950 border border-white/50 outline-none hover:bg-neutral-900 duration-300 text-white/60 ease-in-out px-4 resize-none rounded-2xl" />
+		<form onSubmit={handleSendMessageClick} className="userInput rounded-xl bg-neutral-950 border border-white/50 mt-8 flex items-center justify-between w-150 gap-2">
+			<textarea type="text" value={userMessage} onChange={(e) => setUserMessage(e.target.value)} placeholder="How can I help you today?" className="h-35 w-150 text-sm outline-none text-white/60 resize-none p-3" />
 			
-			<button disabled={!userMessage.trim() || loading} type="submit" className={`border border-white/50 rounded-full p-2.5 text-sky-500 bg-neutral-950 self-start
+			<button disabled={!userMessage.trim() || loading} type="submit" className={`rounded-full p-2 bg-sky-600 self-start mt-2 mr-2
 			${!userMessage.trim() || loading ?
 			'opacity-50' : 
 			'cursor-pointer active:scale-90 duration-300 ease-in-out'}`}>
-				<IoMdArrowRoundUp size={30}/>
+				<IoMdArrowRoundUp color="white" size={30}/>
 			</button>
 		</form>
 	</main>
