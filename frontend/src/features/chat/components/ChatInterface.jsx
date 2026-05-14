9116bbf6cf8b2606b3d49e560a58b319612e38d8
Abdul Ghani <ghanisheikh26@gmail.com>
2026-05-15 01:42:05 +0530

    completed chat interface mobile responsive layout

--- frontend/src/features/chat/components/ChatInterface.jsx
+++ frontend/src/features/chat/components/ChatInterface.jsx
@@ -11,8 +11,8 @@ const ChatInterface = ({messages, handleSendMessageClick, userMessage, setUserMe
         }} className="chatting min-h-screen lg:w-[75%] w-full overflow-y-auto relative flex flex-col items-center lg:px-40 lg:py-5 py-2 gap-2 lg:pb-30 pb-25 px-5">
 
         {messages.map((msg, index) => {
-            return <div key={index} className={`user p-2 rounded-lg text-sm
-            ${msg.role === "ai" ? "w-[80%] self-start" : "w-fit bg-neutral-900 self-end shadow-sm shadow-black/50 rounded-br-none lg:mt-10 mt-3"}`}>
+            return <div key={index} className={`user p-2 rounded-lg lg:text-sm text-xs
+            ${msg.role === "ai" ? "w-[80%] self-start" : "w-fit bg-neutral-900 self-end shadow-sm shadow-black/50 rounded-br-none lg:mt-5 mt-3"}`}>
 
                 {msg.role === "ai" ? (
                     msg.content === "LOADING" ?
--- frontend/src/features/chat/components/InputBox.jsx
+++ frontend/src/features/chat/components/InputBox.jsx
@@ -3,7 +3,7 @@ import { IoMdArrowRoundUp } from "react-icons/io";
 const InputBox = ({handleSendMessage, userMessage, setUserMessage, loading}) => {
   return (
     <footer>
-        <form onSubmit={handleSendMessage} className="fixed -translate-x-[50%] -translate-y-[50%] bottom-0 userInput bg-neutral-950 border border-white/50 lg:w-2/3 w-[90%] flex items-center justify-between gap-5 lg:px-5 lg:py-2 py-1.5 px-3 rounded-full text-xs">
+        <form onSubmit={handleSendMessage} className="fixed -translate-x-[50%] -translate-y-[50%] bottom-0 userInput bg-neutral-950 border border-white/50 lg:w-[60%] w-[90%] flex items-center justify-between gap-5 lg:px-5 lg:py-2 py-1.5 px-3 rounded-full text-xs">
             <input type="text" value={userMessage} onChange={(e) => setUserMessage(e.target.value)} placeholder="Type your message here..." className="w-[85%] text-sm outline-none" />
             
             <button disabled={!userMessage.trim() || loading} type="submit" className={`rounded-full p-1 text-sky-500 bg-sky-600
