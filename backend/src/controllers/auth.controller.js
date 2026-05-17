a1e67ddcfb636720e27277dd98b8979517f3c721
Abdul Ghani <ghanisheikh26@gmail.com>
2026-05-18 01:53:09 +0530

    configed cookies for production

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -138,7 +138,12 @@ export const loginUser = async(req, res) => {
         { expiresIn: "7d" }
     );
     
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 24 * 60 * 60 * 1000
+    });
 
     res.status(200).json({
         success: true,
