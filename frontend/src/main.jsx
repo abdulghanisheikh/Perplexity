b9c4be97afe86e4c731bcbb8e602790bba8eefd2
Abdul Ghani <ghanisheikh26@gmail.com>
2026-06-24 19:05:39 +0530

    Changed font of the application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import "./app/index.css";
 import App from './app/App.jsx';
 import { store } from "./app/app.store.js";
 import { Provider } from "react-redux";
+import '@fontsource/libre-baskerville/400.css';  
+import '@fontsource/libre-baskerville/700.css';
+import '@fontsource/libre-baskerville/400-italic.css';
 
 createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
