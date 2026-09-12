8ada92a601288633502b9a9d52231a4df56a6ae1
Abdul Ghani <ghanisheikh26@gmail.com>
2026-09-12 20:38:57 +0530

    Testing

--- frontend/src/features/auth/service/auth.api.js
+++ frontend/src/features/auth/service/auth.api.js
@@ -4,6 +4,8 @@ const baseURL = import.meta.env.VITE_NODE_ENV === "development" ?
     "http://localhost:3000" :
     import.meta.env.VITE_BACKEND_URL;
 
+console.log("baseURL:", baseURL);
+
 const api = axios.create({
     baseURL: `${baseURL}/api/auth`,
     withCredentials: true
