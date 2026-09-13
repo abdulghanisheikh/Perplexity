d6cb33feb276fce45cf4dcabf29e2e544ae9a1bc
Abdul Ghani <ghanisheikh26@gmail.com>
2026-09-13 20:31:44 +0530

    Rolledback the email service configs.

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -14,8 +14,8 @@ oauth2Client.setCredentials({
 // connection between web server and SMTP (email server)
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         type: "OAuth2",
         user: process.env.GOOGLE_EMAIL_USER,
